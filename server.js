0400f912a0bab33c18658aed28ac8bc1e93d8b20
Derek Caneja <derek@dispatch.me>
2015-04-30 22:10:13 -0400

    Lookup info for the client's IP address, not the server's

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ Meteor.methods({
   'phoneformat.getCountryForIp': function () {
     var response = null;
 
+    // Lookup info for the client's IP address, not the server's
+    var clientIP = this.connection.clientAddress;
+
+    // If phoneformat is configured with a token, do the look up over https
     if (Phoneformat.token) {
-      response = HTTP.get('https://ipinfo.io?token=' + Phoneformat.token);
+      response = HTTP.get('https://ipinfo.io/' + clientIP + '?token=' + Phoneformat.token);
     } else {
-      response = HTTP.get('http://ipinfo.io');
+      response = HTTP.get('http://ipinfo.io/' + clientIP);
     }
 
     return response.data && response.data.country;
@@ -14,6 +18,7 @@ Meteor.methods({
 
 Phoneformat = {};
 
+// Configure phoneformat to use https with a token
 Phoneformat.configure = function (options) {
   options = options || {};
 
