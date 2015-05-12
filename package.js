b4639cfd520cafdb819b8fce36f535baf7604a05
Derek Caneja <derek@dispatch.me>
2015-05-12 17:25:52 -0400

    Bump version to 1.2.4

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'dispatch:phoneformat.js',
   summary: 'phoneformat.js packaged for meteor',
-  version: '1.2.3',
+  version: '1.2.4',
   git: 'https://github.com/DispatchMe/phoneformat.js.git',
 });
 
